8474ab55835eb84a2059b166ceaa99dc9ea24e82
Daniel Craig <taekook0052@gmail.com>
2026-09-16 18:13:29 +0330

    Add FastAPI main server

---
+++ server/main.py
@@ -0,0 +1,18 @@
+﻿from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "AI Agent Gateway is running!"}
+
+@app.get("/providers")
+def list_providers():
+    return {
+        "providers": [
+            "gemini",
+            "groq",
+            "together",
+            "cerebras"
+        ]
+    }
